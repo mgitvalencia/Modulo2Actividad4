89131bb60e41bb1de06d7121b0628f00542ef1c7
mgitvalencia <105307230+mgitvalencia@users.noreply.github.com>
2022-06-25 14:29:27 -0500

    Avance actividad

--- main.py
+++ main.py
@@ -16,13 +16,13 @@ parser.add_argument("-m", "--method", type=str, default="GET", choices=['GET', '
 parser.add_argument("-r", "--resource", type=str, default="posts", choices=['post', 'comments'], required=False , help="Recurso sobre el que se realiza la operación")
 parser.add_argument("-i", "--resource_id", type=str, default="20", required=False, help=" Identificador único del recurso")
 """
-
 args = parser.parse_args()
 
-print(args.method)
-print(args.resource)
-print(args.resource_id)
-
+if (args.method == "DELETE" or args.method == "PUT"):
+    if (args.resource_id == ""):
+        print("Para los metodos DELETE y PUT se requiere el argumento --resource")
+        exit(0)
+        
 
 with open('config.json') as file:
     data = json.load(file)
@@ -33,42 +33,45 @@ time_out = data["request_time_out"]
 res_data = data["response_data"]
 res_status = data["response_status"]
 
-print(url)
-print(time_out)
-print(res_data)
-print(res_status)
-print(url + args.resource)
-
 if (args.method == "GET"):
     if(args.resource_id != ""):
        path = url + args.resource + "/" + args.resource_id
     else:    
        path = url + args.resource
 
-
     headers = {'Content-type': ''}
-    response = requests.get(path, verify=False, headers=headers)
+    response = requests.get(path, headers=headers, timeout=time_out)
     guarda_datos(res_data, response)
     json_data = prepara_json(args.method, path, response)
     guarda_estado(res_status, json_data)
 
-    print(response.headers['Content-Type'])
+if (args.method == "POST"):
+    with open(args.resource + '.json') as file:
+        data = json.load(file)
+    file.close()
+    path = url + args.resource
+    response = requests.post(path, json=data, timeout=time_out)
+    json_data = prepara_json(args.method, path, response)
+    guarda_estado(res_status, json_data)
+    
+if (args.method == "PUT"):
+    with open(args.resource + '_put.json') as file:
+        data = json.load(file)
+    file.close()
+    path = url + args.resource  + "/" + args.resource_id
+    response = requests.put(path, json=data, timeout=time_out)
+    json_data = prepara_json(args.method, path, response)
+    guarda_estado(res_status, json_data)
 
-   
+if (args.method == "DELETE"):
+    path = url + args.resource  + "/" + args.resource_id
+    response = requests.delete(path, timeout=time_out)
+    json_data = prepara_json(args.method, path, response)
+    guarda_estado(res_status, json_data)
+
+
+ 
 
 
 
 
-
-
-"""
-response = requests.get("https://jsonplaceholder.typicode.com/todos/1").text
-objeto = json.loads(response)
-print()
-print(objeto)
-print()
-print(response)
-print()
-print ("Titulo: " + objeto["title"])
-print()
-"""
--- respuesta.py
+++ respuesta.py
@@ -1,13 +1,13 @@
 import json
 
+def guarda_datos(archivo, mensaje):
+    with open(archivo, 'w', encoding=mensaje.encoding) as file:
+       json.dump(json.loads(mensaje.text), file, indent=4)
+    file.close
+
 def guarda_estado(archivo, mensaje):
     with open(archivo, 'w') as file:
        json.dump(mensaje, file, indent=4)
-    file.close
-
-def guarda_datos(archivo, mensaje):
-    with open(archivo, 'w') as file:
-       json.dump(json.loads(mensaje.text), file, indent=4)
     file.close
 
 def prepara_json(metodo, path, response):
@@ -15,7 +15,7 @@ def prepara_json(metodo, path, response):
     json_data['method'] = metodo
     json_data['url'] = path
     json_data['status'] = response.status_code
-    json_data['content-type'] = response.headers['Content-Type']
+    json_data['content-type'] = response.headers['Content-Type'].split(';')[0]
     json_data['encoding'] = response.encoding
     return(json_data)
 
